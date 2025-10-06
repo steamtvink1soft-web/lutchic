29809311e6cb098470871f5340e1523c0e1826dc
steamtvink1soft-web <steamtvink1soft@gmail.com>
2025-10-06 07:19:45 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+# Конфигурация
+TOKEN = '7589790386:AAE0DukAgw0M3aOrarMVF4wezHy3gC_tERQ'
+CREATOR_ID = 7485606858   # ID создателя
+CHAT_LOG_FILE = 'chat_log.txt'
+SCREENSHOTS_FOLDER = 'screenshots'
+comad_bot =(
+    "/starte - запуск сайта\n"
+    "/stop - остановить сайта\n"
+    "/restart - перезапустить сайта\n"
+)
